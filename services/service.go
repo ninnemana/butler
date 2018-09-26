5f7eed9eb0ec40fc4cfad71cd7bb8664ede4c1db
Alex Ninneman <alex@ninneman.org>
2018-09-26 02:17:40 -0500

    adds TLS support

--- services/service.go
+++ services/service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"crypto/tls"
 	"io"
 	"log"
 	"net"
@@ -13,17 +14,14 @@ type Service struct {
 	LocalAddress   string
 	ServiceAddress string
 	Log            bool
+	TLS            *tls.Config
 
 	source net.Listener
-}
-
-func pipe(source *net.TCPConn, dest *net.TCPConn) {
-	defer dest.CloseWrite()
-	defer source.CloseRead()
-	io.Copy(dest, source)
+	dest   net.Conn
 }
 
 func Start(svcs ...*Service) {
+	ch := make(chan bool)
 	for _, svc := range svcs {
 		go func(s *Service) {
 			if err := s.Listen(); err != nil {
@@ -31,6 +29,7 @@ func Start(svcs ...*Service) {
 			}
 		}(svc)
 	}
+	<-ch
 }
 
 func (s *Service) Close() {
@@ -41,7 +40,13 @@ func (s *Service) Listen() error {
 	var err error
 
 	// open TCP listener for source
-	s.source, err = net.Listen("tcp", s.LocalAddress)
+	switch {
+	case s.TLS == nil:
+		s.source, err = net.Listen("tcp", s.LocalAddress)
+	default:
+		s.source, err = tls.Listen("tcp", s.LocalAddress, s.TLS)
+	}
+
 	if err != nil {
 		return errors.Wrap(err, "failed to create TCP listener")
 	}
@@ -55,28 +60,43 @@ func (s *Service) Listen() error {
 		}
 
 		// handle the request
-		go handle(req, s.ServiceAddress)
+		go s.handle(req)
 	}
 }
 
-func handle(req net.Conn, svcAddr string) {
+func (s *Service) handle(req net.Conn) {
 	defer req.Close()
 
+	var err error
 	// dial a connection with the service
-	dest, err := net.Dial("tcp", svcAddr)
+	switch {
+	case s.TLS == nil:
+		s.dest, err = net.Dial("tcp", s.ServiceAddress)
+	default:
+		s.dest, err = tls.Dial("tcp", s.ServiceAddress, s.TLS)
+	}
+
 	if err != nil {
 		log.Printf("failed to connect to service: %v\n", err)
 		return
 	}
-	defer dest.Close()
+	defer s.dest.Close()
 
 	conn := req.(*net.TCPConn)
 	conn.SetKeepAlive(true)
 	conn.SetKeepAlivePeriod(time.Second * 45)
 
 	// pipe the request from the source connection to the destination
-	go pipe(conn, dest.(*net.TCPConn))
+	go pipe(conn, s.dest.(*net.TCPConn))
 
 	// pipe the response from the destination into the response of the source
-	pipe(dest.(*net.TCPConn), conn)
+	pipe(s.dest.(*net.TCPConn), conn)
+}
+
+func pipe(source *net.TCPConn, dest *net.TCPConn) {
+	defer func() {
+		dest.CloseWrite()
+		source.CloseRead()
+	}()
+	io.Copy(dest, source)
 }
