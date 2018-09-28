dfe423e4a914aeca5ce67232d128a18194993174
Alex Ninneman <alex@ninneman.org>
2018-09-27 22:50:15 -0500

    uses reverse proxy HTTP handler

--- services/service.go
+++ services/service.go
@@ -2,12 +2,14 @@ package services
 
 import (
 	"crypto/tls"
-	"fmt"
-	"io"
-	"log"
 	"net"
 	"net/http"
-	"time"
+	"net/http/httputil"
+)
+
+var (
+	hostTarget = map[string]string{}
+	hostProxy  = map[string]*httputil.ReverseProxy{}
 )
 
 type Service struct {
@@ -27,23 +29,30 @@ type Config struct {
 type TLS struct {
 	CertBlock []byte
 	KeyBlock  []byte
+
+	CertFile string
+	KeyFile  string
 }
 
 func Start(cfg Config) error {
 
-	var err error
-	var source net.Listener
-	switch {
-	case cfg.TLS == nil:
-		source, err = net.Listen("tcp", cfg.LocalAddress)
-	default:
+	var tlsConfig *tls.Config
+	if cfg.TLS != nil {
 		var cert tls.Certificate
-		cert, err = tls.X509KeyPair(cfg.TLS.CertBlock, cfg.TLS.KeyBlock)
+		var err error
+
+		switch {
+		case cfg.TLS.CertBlock != nil && cfg.TLS.KeyBlock != nil:
+			cert, err = tls.X509KeyPair(cfg.TLS.CertBlock, cfg.TLS.KeyBlock)
+		case cfg.TLS.CertFile != "" && cfg.TLS.KeyFile != "":
+			cert, err = tls.LoadX509KeyPair(cfg.TLS.CertFile, cfg.TLS.KeyFile)
+		}
+
 		if err != nil {
 			return err
 		}
 
-		source, err = tls.Listen("tcp", cfg.LocalAddress, &tls.Config{
+		tlsConfig = &tls.Config{
 			MinVersion:               tls.VersionTLS12,
 			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 			PreferServerCipherSuites: true,
@@ -54,67 +63,16 @@ func Start(cfg Config) error {
 				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 			},
 			Certificates: []tls.Certificate{cert},
-		},
-		)
-	}
-
-	if err != nil {
-		return err
-	}
-
-	// accept connections for the local address
-	for {
-		req, err := source.Accept()
-		if err != nil {
-			log.Printf("failed to accept connection: %v\n", err)
-			continue
 		}
-
-		fmt.Println(req.RemoteAddr(), req.LocalAddr())
-		conn := req.(*net.TCPConn)
-
-		r := &http.Request{}
-		r.Write(conn)
-		fmt.Println(r)
-		conn.Close()
-		req.Close()
-		// handle the request
-		// go handle(req)
 	}
 
-	return nil
-}
-
-func (s *Service) Close() {
-	s.source.Close()
-}
-
-func (s *Service) handle(req net.Conn) {
-	defer req.Close()
-
-	var err error
-	s.dest, err = net.Dial("tcp", s.ServiceAddress)
-	if err != nil {
-		log.Printf("failed to connect to service: %v\n", err)
-		return
+	h := &handler{}
+	http.Handle("/", h)
+	server := &http.Server{
+		Addr:      cfg.LocalAddress,
+		Handler:   h,
+		TLSConfig: tlsConfig,
 	}
-	defer s.dest.Close()
 
-	conn := req.(*net.TCPConn)
-	conn.SetKeepAlive(true)
-	conn.SetKeepAlivePeriod(time.Second * 45)
-
-	// pipe the request from the source connection to the destination
-	go pipe(conn, s.dest.(*net.TCPConn))
-
-	// pipe the response from the destination into the response of the source
-	pipe(s.dest.(*net.TCPConn), conn)
-}
-
-func pipe(source *net.TCPConn, dest *net.TCPConn) {
-	defer func() {
-		dest.CloseWrite()
-		source.CloseRead()
-	}()
-	io.Copy(dest, source)
+	return server.ListenAndServe()
 }
