de19cf6caaa59f036e8ddbd164c07f66ccf10ee3
Alex Ninneman <alex@ninneman.org>
2018-10-09 22:50:45 -0500

    updates host before proxy

--- services/handler.go
+++ services/handler.go
@@ -18,34 +18,42 @@ type handler struct {
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	host := r.Host
 
+	target, ok := h.Targets[host]
+	if !ok {
+		notFound(w)
+		return
+	}
+
+	remote, err := url.Parse(target)
+	if err != nil {
+		log.Println("target parse fail:", err)
+		return
+	}
+
+	r.Host = remote.Host
+
 	if fn, ok := h.proxies[host]; ok {
 		fn.ServeHTTP(w, r)
 		return
 	}
 
-	if target, ok := h.Targets[host]; ok {
-		remote, err := url.Parse(target)
-		if err != nil {
-			log.Println("target parse fail:", err)
-			return
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+
+	h.l.Lock()
+	switch h.proxies {
+	case nil:
+		h.proxies = map[string]*httputil.ReverseProxy{
+			host: proxy,
 		}
-
-		proxy := httputil.NewSingleHostReverseProxy(remote)
-
-		h.l.Lock()
-		switch h.proxies {
-		case nil:
-			h.proxies = map[string]*httputil.ReverseProxy{
-				host: proxy,
-			}
-		default:
-			h.proxies[host] = proxy
-		}
-		h.l.Unlock()
-
-		proxy.ServeHTTP(w, r)
-		return
+	default:
+		h.proxies[host] = proxy
 	}
+	h.l.Unlock()
+
+	proxy.ServeHTTP(w, r)
+}
+
+func notFound(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusNotFound)
 
 	res, err := http.Get("https://i.pinimg.com/736x/70/01/aa/7001aa5d1483fa70cd00022ed226483d.jpg")
